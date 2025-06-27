997dc59e96dcaf3f8070efb9523291d4581fa77f
Willy De Keyser <spring.boot.programming@gmail.com>
2025-06-27 10:44:11 +0200

    update test.py

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import time
+counter = 0
+print("Start Python Hello world!")
 while True:
-	print("Hello from My Home Lab!")
+	counter+=1
+	print(counter, " Hello World from My Home Lab!")
 	time.sleep(5)
